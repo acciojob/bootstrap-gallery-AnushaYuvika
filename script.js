6c98684fcecc367635f744591ac4c80450a9c583
Anusha Yuvika <151361373+AnushaYuvika@users.noreply.github.com>
2025-09-03 21:24:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const lightboxModal = document.getElementById('lightboxModal');
+const lightboxImage = document.getElementById('lightboxImage');
+
+document.querySelectorAll('.card img').forEach(img => {
+  img.addEventListener('click', function() {
+    lightboxImage.src = this.src; 
+  });
+});
